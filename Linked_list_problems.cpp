1cf8b99a7d6393ac98f6e5b9156da78ba4c8e364
StanleyHristov <stenlihr4@gmail.com>
2025-06-06 21:54:18 +0300

    new version with problematics

--- Linked_list_problems.cpp
+++ Linked_list_problems.cpp
@@ -35,6 +35,17 @@ void appendLast(Node* head , int data)
     new_node->next = nullptr;
 }
 
+void print(Node* head)
+{
+    Node* temp = head;
+    while(temp != nullptr)
+    {
+        cout<< temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<"End"<<endl;
+}
+
 
 void GetNth(Node* head , int index)
 {
@@ -46,8 +57,24 @@ void GetNth(Node* head , int index)
         count++;
     } 
     cout<<temp->data;
+    
+    
 }
-
+void InsertNth(Node* head , int index , int data){
+    Node* temp = head;
+    
+    int count = 1;
+    while (count!=index)
+    {
+        temp = temp->next;
+        count++;
+    } 
+    Node* current = temp;
+    Node* new_node = create_node(data);
+    temp->next = new_node;
+    new_node->next = current->next->next;
+    
+}
 
 
 
@@ -58,7 +85,9 @@ int main()
 Node* head = create_node(5);
 appendLast(head , 20);
 appendLast(head , 25);
+InsertNth(head , 3 , 1);
 appendLast(head , 24);
 GetNth(head , 3);
+print(head);
 
 }
